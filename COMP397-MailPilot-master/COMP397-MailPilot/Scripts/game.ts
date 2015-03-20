8cb01040e1c91860cc49696c282aaec9f8cec1e5
zirgham <mohd.zirgham@gmail.com>
2015-03-20 15:01:45 -0400

    updated gameplay screen + audio + image

---
+++ COMP397-MailPilot-master/COMP397-MailPilot/Scripts/game.ts
@@ -0,0 +1,131 @@
+﻿/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+
+/// <reference path="typings/stats/stats.d.ts" />
+
+/// <reference path="constants.ts" />
+/// <reference path="objects/gameobject.ts" />
+/// <reference path="objects/scoreboard.ts" />
+/// <reference path="objects/plane.ts" />
+/// <reference path="objects/island.ts" />
+/// <reference path="objects/cloud.ts" />
+/// <reference path="objects/ocean.ts" />
+/// <reference path="objects/button.ts" />
+/// <reference path="objects/label.ts" />
+
+/// <reference path="states/gameplay.ts" />
+/// <reference path="states/gameover.ts" />
+/// <reference path="states/menu.ts" />
+
+
+// Global game Variables
+var canvas;
+var stage: createjs.Stage;
+var assetLoader: createjs.LoadQueue;
+var stats: Stats = new Stats();
+var currentScore = 0;
+var highScore = 0;
+
+
+// Game State Variables
+var currentState: number;
+var currentStateFunction: any;
+var stateChanged: boolean = false;
+
+var gamePlay: states.GamePlay;
+var gameOver: states.GameOver;
+var menu: states.Menu;
+
+
+var manifest = [
+    { id: "cloud", src: "assets/images/asteroid.png" },
+    { id: "island", src: "assets/images/star.png" },
+    { id: "ocean", src: "assets/images/space.jpg" },
+    { id: "plane", src: "assets/images/spaceship.png" },
+    { id: "playButton", src: "assets/images/playGameButton.png" },
+    { id: "tryAgainButton", src: "assets/images/playAgainButton.png" },
+    { id: "engine", src: "assets/audio/star.ogg" },
+    { id: "yay", src: "assets/audio/powerup.wav" },
+    { id: "thunder", src: "assets/audio/collision.wav" }
+];
+
+
+function Preload() {
+    assetLoader = new createjs.LoadQueue(); // create a new preloader
+    assetLoader.installPlugin(createjs.Sound); // need plugin for sounds
+    assetLoader.on("complete", init, this); // when assets finished preloading - then init function
+
+    assetLoader.loadManifest(manifest);
+}
+
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    setupStats();
+
+    currentState = constants.MENU_STATE;
+    changeState(currentState);
+}
+
+function setupStats() {
+    stats.setMode(0); 
+
+    // align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '650px';
+    stats.domElement.style.top = '450px';
+
+    document.body.appendChild(stats.domElement);
+}
+
+
+function gameLoop() {
+    stats.begin();
+
+    if (stateChanged) {
+        changeState(currentState);
+        stateChanged = false;
+    }
+    else {
+        currentStateFunction.update();
+    }
+        
+    stats.end();
+}
+
+
+function changeState(state: number): void {
+    // Launch Various "screens"
+    switch (state) {
+        case constants.MENU_STATE:
+            // instantiate menu screen
+            menu = new states.Menu();
+            currentStateFunction = menu;
+            break;
+
+        case constants.PLAY_STATE:
+            // instantiate game play screen
+            gamePlay = new states.GamePlay();
+            currentStateFunction = gamePlay;
+            break;
+
+        case constants.GAME_OVER_STATE:
+            // instantiate game over screen
+            gameOver = new states.GameOver();
+            currentStateFunction = gameOver;
+            break;
+    }
+}
+
+
+
+
+
+
---
+++ COMP397-MailPilot-master/COMP397-MailPilot/Scripts/objects/plane.ts
@@ -0,0 +1,29 @@
+﻿
+module objects {
+    // PLANE CLASS
+    export class Plane extends createjs.Bitmap {
+        public width: number;
+        public height: number;
+        // CONSTRUCTOR
+        constructor() {
+            super(assetLoader.getResult("plane"));
+
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+
+            this.y = 220;
+            this.x = 100;
+            this.regX = this.width * 0.5;
+            this.regY = this.height * 0.5;
+            createjs.Sound.play("engine", {loop: -1});
+        }
+
+        // PUBLIC METHODS
+        public update() {
+
+            this.y = stage.mouseY;
+        }
+
+    }
+
+} 
